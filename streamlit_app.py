56a86da3dd8083652013220a7e69c1e4ad8ac294
hannah.r.kerner@gmail.com <hannah-rae>
2023-08-19 11:18:28 -0700

    Add Kula data and update text info/formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from streamlit_folium import st_folium
 
 import os
 import geopandas as gpd
+import pandas as pd
 from geopy.geocoders import GoogleV3
 from shapely.geometry import Point
 
@@ -26,29 +27,40 @@ class Verifier:
     # Load the burn scar polygons
     self.burn_scar = gpd.read_file(root / "burn-area.geojson")
     # Load building damage polygons
-    self.bld_dmg = gpd.read_file(root / "lahaina-building-damage.geojson").to_crs('EPSG:4326')
+    lh_bld_dmg = gpd.read_file(root / "lahaina-building-damage.geojson")
+    lh_bld_dmg.crs = 32604
+    ku_bld_dmg = gpd.read_file(root / "kula-building-damage.geojson")
+    ku_bld_dmg.crs = 32604
+    ku_bld_dmg['damaged'] = 1
+    bld_dmg = pd.concat([ku_bld_dmg, lh_bld_dmg])
+    self.bld_dmg = bld_dmg.to_crs(epsg=4326)
 
   def check_in_burn_scar(self):
     # Check if address is contained within burn scar
-    if self.burn_scar.iloc[1].geometry.contains(self.addr_point).any():
-      st.write('Address %s is inside South Maui/Upcountry burn scar area' % self.location.address)
-    elif self.burn_scar.iloc[0].geometry.contains(self.addr_point).any():
-      st.write('Address %s is inside Lahaina burn scar area' % self.location.address)
+    if self.addr_point.geometry.within(self.burn_scar.iloc[1].geometry).any():
+      st.write("Address %s is inside South Maui/Upcountry burn scar area" % self.location.address)
+    elif self.addr_point.geometry.within(self.burn_scar.iloc[0].geometry).any():
+      st.write("Address %s is inside Lahaina burn scar area" % self.location.address)
     else:
-      st.write('Address %s is NOT inside either burn scar area' % self.location.address)
+      st.write("Address %s is NOT inside either burn scar area" % self.location.address)
 
   def check_in_building_damage(self):
-    st.write("NOTE: Building damage detection and damage levels based on imagery from August 9. Further damage may have occurred that is not shown here.\nSee NOAA website for latest high-res imagery: https://storms.ngs.noaa.gov/storms/2023_hawaii/index.html")
+    bldg_warning = """NOTE: Building damage detection and damage levels based on imagery from August 9
+                      provided by Microsoft AI for Good Geospatial (POC: Caleb Robinson).
+                      Further damage may have occurred that is not shown here.\nSee NOAA website for
+                      latest high-res imagery: https://storms.ngs.noaa.gov/storms/2023_hawaii/index.html"""
     # Check if the address matches a building and if so what the damage level is
     bldg_match = False
     for idx, row in self.bld_dmg.iterrows():
-      if row.geometry.contains(self.addr_point).any():
+      if self.addr_point.geometry.within(row.geometry).any():
         bldg_match = True
         st.write('Address %s matches building in building damage detection database' % self.location.address)
-        st.write('Estimated damage level: %f percent' % (float(row['damage_pct'])*100))
+        if row['damage_pct'] > 0:
+          st.write('Estimated damage level: {0:.2f}%'.format(float(row['damage_pct'])*100))
         break
     if not bldg_match:
       st.write('Address %s does not match any buildings in building damage detection database' % self.location.address)
+    st.write(bldg_warning)
 
   def display_map(self):
     # Display the map
@@ -65,14 +77,20 @@ class Verifier:
 
     folium.TileLayer(
         tiles="https://geospatialvisualizer.z13.web.core.windows.net/tiles/skysat_maui_8_10_2023_rgb_tiles/{z}/{x}/{y}.png",
-        name='SkySat post-fire August 9',
-        attr="SkySat"
+        name='SkySat Lahaina post-fire August 9',
+        attr="SkySat Lahaina"
     ).add_to(map)
 
     folium.TileLayer(
       tiles="https://geospatialvisualizer.z13.web.core.windows.net/tiles/10300100EB592000_tiles/{z}/{x}/{y}.png",
-      name='Maxar post-fire August 9',
-      attr="Maxar"
+      name='Maxar Lahaina post-fire August 9',
+      attr="Maxar Lahaina"
+    ).add_to(map)
+
+    folium.TileLayer(
+      tiles="https://geospatialvisualizer.z13.web.core.windows.net/tiles/10503F0006FE4B00_tiles/{z}/{x}/{y}.png",
+      name='Maxar Kula post-fire August 9',
+      attr="Maxar Kula"
     ).add_to(map)
 
     geo_j = self.burn_scar.to_json()
@@ -83,13 +101,13 @@ class Verifier:
 
     builds_geo_j =  self.bld_dmg[self.bld_dmg['damaged']==1].to_json()
     builds_geo_j = folium.GeoJson(data=builds_geo_j,
-                                  style_function=lambda x: {'color': 'black', 'fillColor': 'red', 'opacity':0.5, 'fillOpacity':0.6},
+                                  style_function=lambda x: {'color': 'black', 'fillColor': 'red', 'opacity':0.5, 'fillOpacity':0.4},
                                   name="Damaged buildings")
     builds_geo_j.add_to(map)
 
     intact_geo_j =  self.bld_dmg[self.bld_dmg['damaged']==0].to_json()
     intact_geo_j = folium.GeoJson(data=intact_geo_j,
-                                  style_function=lambda x: {'color': 'black', 'fillColor': 'green', 'opacity':0.5, 'fillOpacity':0.6},
+                                  style_function=lambda x: {'color': 'black', 'fillColor': 'green', 'opacity':0.5, 'fillOpacity':0.4},
                                   name="Intact buildings")
     intact_geo_j.add_to(map)
 
@@ -102,6 +120,14 @@ class Verifier:
 api_key = os.environ.get("api_key", None)
 
 with st.form("inputs"):
+  info_text = """This app is used to check if a user-specified address is inside
+                 the Lahaina or Kula/South Maui burn scars and if the
+                 address matches a building in the building damage detection
+                 database. The address will be plotted on a map with geospatial and
+                 satellite data layers showing impacts of the wildfires."""
+  st.write(info_text)
+  st.write("""If you experience any issues with the app, please contact Hannah
+              Kerner at hkerner@asu.edu.""")
   st.write("Input the following:")
   if api_key is None:
     api_key = st.text_input("Input your google maps key")
